c24d073be987038a2ef8eb4188c3f413ce30477f
yen <l.yennie.j@gmail.com>
2024-01-23 14:46:52 +0900

    server read

--- app/read/[id]/page.js
+++ app/read/[id]/page.js
@@ -1,8 +1,14 @@
-export default function Read(props) {
+export default async function Read(props) {
+  const id = props.params.id;
+  const resp = await fetch(`http://localhost:9999/topics/${id}`);
+  const topic = await resp.json();
+
   return (
     <>
-      <h2>Read</h2>
-      params : {props.params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
+
+// 페이지가 사용자와 상호작용하는가(client) vs 데이터를 읽고 보여주기만 하는가(server)
